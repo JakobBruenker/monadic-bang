7ce378d65201808fa3fc16dd53bf6915e59d15b2
Jakob Bruenker <jakob.bruenker@gmail.com>
2022-04-03 05:35:48 +0200

    make monadic

--- src/MonadicBang.hs
+++ src/MonadicBang.hs
@@ -12,9 +12,12 @@
 module MonadicBang (plugin) where
 
 import Control.Arrow
+import Control.Monad.Trans.RWS.CPS
 import Data.Data
 import Data.Function
 import Data.Functor
+import Data.Map.Strict (Map)
+import Data.Map.Strict qualified as M
 import GHC
 import GHC.Data.Bag
 import GHC.Parser.Errors.Types
@@ -31,7 +34,7 @@ plugin = defaultPlugin
 
 -- We don't care about which file things are from, because the entire AST comes
 -- from the same module
-data Loc = MkLoc {line :: !Int, col :: Int}
+data Loc = MkLoc {line :: Int, col :: Int}
          deriving (Eq, Ord, Show)
 
 -- | Increment column by one to get the location after a bang
@@ -50,7 +53,7 @@ replaceBangs _ _ (ParsedResult (HsParsedModule lexp files) msgs) =
   pure $ ParsedResult (HsParsedModule (fillHoles fills lexp) files) msgs{psErrors}
   where
     -- Take out the errors we care about, throw the rest back in
-    (mkMessages -> psErrors, bagToList -> fills) =
+    (mkMessages -> psErrors, M.fromList . bagToList -> fills) =
       flip partitionBagWith msgs.psErrors.getMessages \cases
         err
           | PsErrBangPatWithoutSpace ExprLoc{loc, expr} <- err.errMsgDiagnostic
@@ -59,8 +62,12 @@ replaceBangs _ _ (ParsedResult (HsParsedModule lexp files) msgs) =
 
 -- | Replace holes in an AST whenever an expression with the corresponding
 -- source span can be found in the given list.
---
--- Expressions to fill them with are taken from the errors.
+fillHoles :: forall a . Data a => Map Loc (HsExpr GhcPs) -> a -> a
+fillHoles fillers ast = case runRWS (go ast) () fillers of
+  -- TODO: throw error if remaining isn't empty
+  (ast, remaining, stmts) -> ast
+  where
+
 -- TODO: embed the expression in existing or new do-notation
 -- Approach: in tryFillHole, whenever we encounter let/where/do/etc.,
 -- make a separate monadic traversal through the subtree with a writer monad,
@@ -68,11 +75,18 @@ replaceBangs _ _ (ParsedResult (HsParsedModule lexp files) msgs) =
 -- correct do block once the traversal is evaluated
 -- TODO for large modules with lots of !s, this might be slightly faster if we
 -- only consider bangs we haven't found yet, i.e. remove others from the list.
-fillHoles :: forall a . Data a => [(Loc, HsExpr GhcPs)] -> a -> a
-fillHoles exprs = gmapT \cases
-  (e :: e) -> eqT @e @(LHsExpr GhcPs) & \cases
-    (Just Refl)
-      | lexp@(ExprLoc (dropBang -> loc) (HsUnboundVar _ _)) <- e
-      , Just expr <- lookup loc exprs
-      -> fillHoles exprs (lexp $> expr)
-    _ -> fillHoles exprs e
+    go :: forall a . Data a => a -> Fill a
+    go = gmapM \cases
+      (e :: e) -> eqT @e @(LHsExpr GhcPs) & \cases
+        (Just Refl)
+          | lexp@(ExprLoc (dropBang -> loc) (HsUnboundVar _ _)) <- e
+          , Just expr <- fillers M.!? loc
+          -> go (lexp $> expr)
+        _ -> go e
+
+-- TODO add something to state that lets us generate new variable names (hmm how do we make sure they don't clash with anything? If they're long enough I guess that would be sufficient)
+-- This might just be IO btw, need not be state - although random isn't a boot package... and I'd rather only rely on those
+-- this would be easier if we were in TcM
+-- anyway take a look at unsafeGetFreshLocalUnique - don't think we can use it though
+-- we don't technically even *need* randomness - we could start with one randomly generated number that we then use for every module, and increment by one every time
+type Fill = RWS () [(RdrName, HsExpr GhcPs)] (Map Loc (HsExpr GhcPs))
--- test/MonadicBangTest.hs
+++ test/MonadicBangTest.hs
@@ -18,3 +18,27 @@ assertEq expected actual
 
 bangWithoutDo :: HasCallStack => IO ()
 bangWithoutDo = assertEq "monadic-bang-test" !getProgName
+
+-- TODO:
+-- let; in Idris the do block is around the entire let expression I don't know if I like that though?
+--    There's an infelicity here: on the one hand, it's really useful to have ! not introduce a new do-block inside let inside do
+--    On the other hand, it would be nice if let inside do worked like let...in, and if that worked like where
+--    But where *has* to work like top-level function definitions
+--    In any case, it's probably a good idea to stick to the idris conventions for now
+-- where
+-- multiply nested
+-- mdo
+-- rec
+-- do
+-- lambda
+-- case scrutinee
+-- case body
+-- case where (treat the same as top level?)
+-- view pattern -> seems kinda hard but doable (that is on top level, apart from that it's the same as everything else)
+-- let inside do; In idris this uses the existing do-block, so
+-- do let a = !getLine
+--    print a
+-- here (a :: String), *not* (a :: IO String)
+
+-- The one case which I think I won't handle like idris is that for us, a bare !x expression at top level will be treated as do {x' <- x; pure x}
+-- which is equivalent to x. It's a type error in idris. Alternatively we could make it a parse error... since it's not like there's any point in doing it.
